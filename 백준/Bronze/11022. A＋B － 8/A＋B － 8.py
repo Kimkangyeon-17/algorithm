63adec06ed2ebbdb0d707037d8b4cd7b5655a96c
Kimkangyeon-17 <157786289+Kimkangyeon-17@users.noreply.github.com>
2024-08-27 17:30:21 +0900

    [Bronze V] Title: A+B - 8, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input().strip())
+
+results = []
+
+for i in range(1, T + 1):
+    A, B = map(int, input().strip().split())
+    results.append(f"Case #{i}: {A} + {B} = {A + B}")
+
+print("\n".join(results))
