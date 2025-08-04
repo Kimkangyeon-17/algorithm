d4ffbda878b5333e3cf602db40d4ea5803d46b1b
Kim kangyeon <157786289+Kimkangyeon-17@users.noreply.github.com>
2025-08-04 22:23:29 +0900

    [Bronze II] Title: 블랙잭, Time: 88 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,14 +1,14 @@
-n, m = map(int,input().split())
-cards = list(map(int,input().split()))
+N, M = map(int, input().split())
+
+cards = list(map(int, input().split()))
 
 best_sum = 0
-
-for i in range(n):
-    for j in range(i+1, n):
-        for k in range(j+1, n):
+for i in range(N):
+    for j in range(i + 1, N):
+        for k in range(j + 1, N):
             card_sum = cards[i] + cards[j] + cards[k]
 
-            if card_sum <= m:
-                best_sum = max(best_sum, card_sum)
+            if card_sum <= M:
+                best_sum = max(card_sum, best_sum)
 
 print(best_sum)
