4fca09300faa151d539bce1ed2d7ea3b6e413724
Kim kangyeon <157786289+Kimkangyeon-17@users.noreply.github.com>
2025-08-17 22:22:20 +0900

    [Bronze I] Title: 색종이, Time: 1328 ms, Memory: 39196 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,25 +1,21 @@
-table = [[0] * 1001 for _ in range(1001)]
+import sys
+input = sys.stdin.readline
 
 N = int(input())
+table = [[0] * 1001 for _ in range(1001)]
 
 for i in range(N):
-    x1, y1, x2, y2 = list(map(int, input().split()))
+    x1, y1, width, height = map(int, input().split())
 
-    for x in range(x1, x1 + x2):
-        for y in range(y1, y1 + y2):
-            table[x][y] = (i+1)
+    for y in range(y1, y1 + height):
+        table[y][x1:(x1 + width)] = [i + 1] * width
 
-result = []
 
 for i in range(N):
     target_num = i + 1
     total = 0
 
-    for col in range(1001):
-        for row in range(1001):
-            if table[col][row] == target_num:
-                total += 1
-    result.append(total)
+    for row in range(1001):
+        total += table[row].count(target_num)
 
-for r in result:
-    print(r)
+    print(total)
