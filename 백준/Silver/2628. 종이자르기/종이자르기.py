2d3295929f9e390ff8834c124a43e93ef8c72b23
Kim kangyeon <157786289+Kimkangyeon-17@users.noreply.github.com>
2025-09-27 21:33:27 +0900

    [Silver V] Title: 종이자르기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2628. 종이자르기/종이자르기.py
+++ 백준/Silver/2628. 종이자르기/종이자르기.py
@@ -1,31 +1,26 @@
 col, row = map(int, input().split())
+
 N = int(input())
 
-row_lst = []
-col_lst = []
+rows, cols = [0, row], [0, col]
 
 for _ in range(N):
     num, w = map(int, input().split())
     if num == 0:
-        row_lst.append(w)
+        rows.append(w)
     else:
-        col_lst.append(w)
+        cols.append(w)
 
-row_lst.extend([0, row])
-col_lst.extend([0, col])
+rows, cols = sorted(set(rows)), sorted(set(cols))
 
-row_lst = sorted(list(set(row_lst)))
-col_lst = sorted(list(set(col_lst)))
+max_row = 0
+for i in range(len(rows) - 1):
+    length = rows[i + 1] - rows[i]
+    max_row = max(max_row, length)
 
-max_row_gap = 0
-for i in range(len(row_lst)-1):
-    gap = row_lst[i+1] - row_lst[i]
-    max_row_gap = max(max_row_gap, gap)
+max_col = 0
+for i in range(len(cols) - 1):
+    length = cols[i + 1] - cols[i]
+    max_col = max(max_col, length)
 
-max_col_gap = 0
-for i in range(len(col_lst)-1):
-    gap = col_lst[i+1] - col_lst[i]
-    max_col_gap = max(max_col_gap, gap)
-
-print(max_row_gap * max_col_gap)
-
+print(max_row * max_col)
